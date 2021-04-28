2ac08048a5b3600d47e5b2eb090f9e5ddc02e0bc
bharathtakkalki <bharathtakkalki@gmail.com>
2021-04-28 21:56:08 +0530

    Complete Calculator - add & Subtract flow complete

--- calculatorApp/calculator.js
+++ calculatorApp/calculator.js
@@ -7,10 +7,10 @@ class Calculator{
         this.operation = ""
     }
     add(){
-        this.result = parseInt(this.numOne) + parseInt(this.numTwo)
+        this.result = Number(this.numOne) + Number(this.numTwo)
     }
     subtract(){
-        this.result = this.numOne - this.numTwo
+        this.result =  Number(this.numOne) -  Number(this.numTwo)
     }
     setNumOne(value){
         this.numOne += value
@@ -20,6 +20,28 @@ class Calculator{
     }
     setOperation(operation){
         this.operation = operation
+    }
+    clearCalculator(){
+        this.numOne = ""
+        this.numTwo = ""
+        this.result = ""
+        this.prevResult = ""
+        this.operation = ""
+    }
+    changeSign(forNumOne){
+        if(forNumOne){
+            this.numOne = String(Number(this.numOne) * -1)
+            return
+        }
+        this.numTwo =  String(Number(this.numTwo) * -1)
+    }
+    undo(forNumOne){
+        if(forNumOne){
+            this.numOne =  this.numOne.substring(0,(this.numOne.length-1))
+            return;
+        }
+        this.numTwo =  this.numTwo.substring(0,(this.numTwo.length-1))
+
     }
 }
 
@@ -39,15 +61,14 @@ const calculator = new Calculator()
 
 const updateMainDisplay = () =>{
     const main = document.getElementById("main-display")
-    main.innerText =  calculator.operation ? (calculator.numTwo ? calculator.numTwo : 0) : calculator.numOne
+    main.innerText =  calculator.operation ? (calculator.numTwo || 0) : (calculator.numOne || 0)
    
 }
 
 const updateSecondaryDisplay = () =>{
     const secondary = document.getElementById("secondary-display")
     secondary.innerHTML = ` ${calculator.numOne} <span class="operand">${SignObject[calculator.operation] ? SignObject[calculator.operation] : ""}</span> ${calculator.numTwo}`
-
-}
+} 
 const numKeyClickHandler = (num) =>{
     if(calculator.operation){  
         calculator.setNumTwo(num)
@@ -56,7 +77,6 @@ const numKeyClickHandler = (num) =>{
     }
     updateMainDisplay()
     updateSecondaryDisplay()
-    console.log(calculator.numOne)
 }
 
 const operandKeyClickHandler = (operation) =>{
@@ -80,3 +100,23 @@ const resultClickHandler = () =>{
     main.innerText = calculator.result
 }
 
+const actionKeyClickHandler = (action) => {
+    switch (action) {
+        case "CLEAR_ALL":
+            calculator.clearCalculator()
+            break;
+        case "CHANGE_SIGN":
+            calculator.changeSign(calculator.operation ? false : true)
+            break;
+        default:
+            break;
+    }
+    updateMainDisplay()
+    updateSecondaryDisplay()
+}
+
+const undoClickHandler = () =>{
+    calculator.undo(calculator.operation ? false : true)
+    updateMainDisplay()
+    updateSecondaryDisplay()
+}
